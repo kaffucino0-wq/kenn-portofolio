9c0978f4aa3beed7b698ddaa1465a304e9ab0f62
kaffucino0-wq <kaffu.cino0@gmail.com>
2026-04-03 10:35:17 +0700

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,17 @@
+
+export const data = {
+  name: "Mochammad Keenandaru Jeza Latjeno",
+  role: "Digital Creative",
+  about: "I am a passionate Digital Creative with a strong background in graphic design, content creation, digital marketing.",
+
+  portfolio: [
+    { title: "Photography", image: "https://source.unsplash.com/600x400/?photography" },
+    { title: "Design", image: "https://source.unsplash.com/600x400/?design" },
+    { title: "Cinematography", image: "https://source.unsplash.com/600x400/?cinema" }
+  ],
+
+  contact: {
+    email: "kaffucino0@gmail.com",
+    phone: "62895360858881"
+  }
+}
